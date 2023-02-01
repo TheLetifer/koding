056c9795d1d7a8e10fa0d62701f41f6f6f51e5a5
TheLetifer <adityaverma.0412@gmail.com>
2023-02-01 23:39:03 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = """
+    <html>
+        <head>
+            <style>
+                .center {
+                    text-align: center;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="center">
+                <p>Hello!</p>
+            </div>
+        </body>
+    </html>
+    """
+    return html
+
+
+
+if __name__ == "__main__":
+    app.run()
+
