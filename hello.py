286c2d8141b1e8594f8f50e3395db4c971700b47
TheLetifer <adityaverma.0412@gmail.com>
2023-02-02 14:58:49 +0530

    pwaapp

--- hello.py
+++ hello.py
@@ -1,5 +1,40 @@
 from flask import Flask, request
 app = Flask(__name__)
+app.index_string = '''<!DOCTYPE html>
+<html>
+<head>
+<title>My app title</title>
+<link rel="manifest" href="./assets/manifest.json" />
+{%metas%}
+{%favicon%}
+{%css%}
+</head>
+<script type="module">
+   import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+   const el = document.createElement('pwa-update');
+   document.body.appendChild(el);
+</script>
+<body>
+<script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', ()=> {
+      navigator
+      .serviceWorker
+      .register('./assets/sw01.js')
+      .then(()=>console.log("Ready."))
+      .catch(()=>console.log("Err..."));
+    });
+  }
+</script>
+{%app_entry%}
+<footer>
+{%config%}
+{%scripts%}
+{%renderer%}
+</footer>
+</body>
+</html>
+'''
 
 @app.route("/")
 def hello():
